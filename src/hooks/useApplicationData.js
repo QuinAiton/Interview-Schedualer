0d7797833eac6ad61a89812ae3931249c4ccfd14
quinaiton <quinaiton@hotmail.com>
2021-02-20 12:04:28 -0800

    Moves All State Management to Custom Hook

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useApplicationData = () => {
+  // State
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: [],
+  });
+
+  //API Managment
+  useEffect(() => {
+    Promise.all([
+      axios.get('api/days'),
+      axios.get('api/appointments'),
+      axios.get('api/interviewers'),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, [state]);
+
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios.put(`api/appointments/${id}`, { interview }).then((data) => {
+      setState({ ...state, appointments });
+    });
+  };
+
+  const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios
+      .delete(`api/appointments/${id}`, { data: { appointments } })
+      .then((data) => {
+        console.log(data);
+        setState({ ...state, appointments });
+      });
+  };
+
+  const setDay = (day) => setState((cur) => ({ ...cur, day }));
+
+  return { state, bookInterview, cancelInterview, setDay };
+};
